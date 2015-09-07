a686603b1705063d32cfab4d6a10984307f7d81b
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-09-07 14:26:09 +0300

    done

--- js/common.js
+++ js/common.js
@@ -129,4 +129,27 @@ head.ready(function() {
 	    return false;
 	});
 
+	function tab() {
+        $('.js-tab-group').each(function(){
+            var tab_cont = $(this).find('.js-tab-cont'),
+                tab_link = $(this).find('.js-tab-link');
+
+            tab_link.first().addClass('is-active');
+            tab_cont.hide();
+            tab_cont.first().show();
+
+            $('body').on('click', '.btn-tab, .js-tab-link', function(){
+                var link = $(this).attr('href');
+                var activeTab = $(this).parents('.js-tab-group').find('.' + link);
+                $(this).parents('.js-tab-group').find('.btn-tab,.js-tab-link').removeClass('is-active');
+                $(this).addClass('is-active');
+                $(this).parents('.js-tab-group').find('.js-tab-cont').hide();
+                activeTab.show();
+
+                return false;
+            });
+        });
+    }
+    tab();	
+
 });
